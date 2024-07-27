19a8dfbfb0d467e3f28255b86b7aa085f412c340
Manish Verma <120708310+ManishVerma0000@users.noreply.github.com>
2024-07-27 23:44:10 +0530

    new comit

---
+++ src/components/Carasoul.vue
@@ -0,0 +1,28 @@
+<template>
+    <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img
+      src="../assets/store.jpeg"
+      className="w-full" />
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+ 
+  
+
+</div>
+</template>
+
+
+
+<script>
+export default {
+    data(){
+        return
+
+    }
+}
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,27 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-import App from './App.vue'
-import Login from './components/Login.vue'
-import Dashboard from './components/Dashboard.vue'
-import Signup from './components/Signup.vue'
+import App from "./App.vue";
+import Login from "./components/Login.vue";
+import Dashboard from "./components/Dashboard.vue";
+import Signup from "./components/Signup.vue";
+import Navigation from "./components/Navigation.vue";
+import Main from "./components/Main.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        
-        { path: '/', component: Login },
-        { path: '/Dashboard', component: Dashboard },
-        { path: '/Signup', component: Signup },
-        
-        
-    ]
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Main },
+    { path: "/Dashboard", component: Dashboard },
+    { path: "/Signup", component: Signup },
+    { path: "/Navigation", component: Navigation },
+  ],
 });
 
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(router);
 
-
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
